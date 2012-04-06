2c3c93df9e2422493151efe45ce687a66df1bb96
Ryan Waldron <rew@erebor.com>
2012-04-06 18:17:15 -0500

    Update kindlegen to point to latest version of KindleGen (2.4)

--- non-free/kindlegen.rb
+++ non-free/kindlegen.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Kindlegen < Formula
-  url 'http://s3.amazonaws.com/kindlegen/KindleGen_Mac_i386_v2.zip'
+  url 'http://s3.amazonaws.com/kindlegen/KindleGen_Mac_i386_v2_4.zip'
   homepage 'http://www.amazon.com/gp/feature.html?docId=1000234621'
-  md5 '047940fa927751ca426e0cfab0f30815'
-  version '2.3'
+  md5 '48c726f03082ad334b811b26adebac5a'
+  version '2.4'
 
   skip_clean 'bin'
 
